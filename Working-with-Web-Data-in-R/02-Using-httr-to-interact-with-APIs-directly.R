e7727c460044347a3a06db454f3ff2e692089740
Vitor Borges Jr <vntborgesjr@gmail.com>
2020-11-09 23:35:10 -0300

    Create 02-Using-httr-to-interact-with-APIs-directly.R

---
+++ Working-with-Web-Data-in-R/02-Using-httr-to-interact-with-APIs-directly.R
@@ -0,0 +1,7 @@
+# --------------------------------------------------- 
+# Working with Web Data in R - Using httr to interact with APIs directly 
+# 09 nov 2020 
+# VNTBJR 
+# --------------------------------------------------- 
+#
+# GET and POST requests in theory  -------------------------------------------
