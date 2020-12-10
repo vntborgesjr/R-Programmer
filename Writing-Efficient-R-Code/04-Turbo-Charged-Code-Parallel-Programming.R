0a351aa92130e8d1e1239a1114a0ad386aef0f10
Vitor Borges Jr <vntborgesjr@gmail.com>
2020-12-10 16:37:43 -0300

    Create 04-Turbo-Charged-Code-Parallel-Programming.R

---
+++ Writing-Efficient-R-Code/04-Turbo-Charged-Code-Parallel-Programming.R
@@ -0,0 +1,8 @@
+# --------------------------------------------------- 
+# Writing Efficient R Code - Turbo Charged Code: Parallel Programming 
+# 10 dez 2020 
+# VNTBJR 
+# --------------------------------------------------- 
+#
+# CPUs- why do we have more than one  -------------------------------------------
+# How many cores does this machine have?
