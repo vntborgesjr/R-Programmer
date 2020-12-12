67c0155b4840ef5113e88d612baafe275b2b3019
Vitor Borges Jr <vntborgesjr@gmail.com>
2020-12-12 19:17:39 -0300

    Update 03-Pattern-matching-with-regular-expressions.R

--- String-Maniuplation-with-stringr-in-R/03-Pattern-matching-with-regular-expressions.R
+++ String-Maniuplation-with-stringr-in-R/03-Pattern-matching-with-regular-expressions.R
@@ -1,5 +1,6 @@
 # --------------------------------------------------- 
-# String Manipulation with stringr in R - Pattern matching with regular expressions 
+# String Manipulation with stringr in R - Pattern matching with
+# regular expressions 
 # 12 dez 2020 
 # VNTBJR 
 # --------------------------------------------------- 
