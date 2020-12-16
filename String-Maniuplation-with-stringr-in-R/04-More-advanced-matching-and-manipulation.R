d17e5717ebaf68b7bdc8d26d02341f6c12e6aefc
Vitor Borges Jr <vntborgesjr@gmail.com>
2020-12-16 00:12:20 -0300

    Update 04-More-advanced-matching-and-manipulation.R

--- String-Maniuplation-with-stringr-in-R/04-More-advanced-matching-and-manipulation.R
+++ String-Maniuplation-with-stringr-in-R/04-More-advanced-matching-and-manipulation.R
@@ -1,6 +1,8 @@
 # --------------------------------------------------- 
-# String Manipulation with stringr in R -  
+# String Manipulation with stringr in R - More advanced matching 
+# and manipulation 
 # 12 dez 2020 
 # VNTBJR 
 # --------------------------------------------------- 
 #
+# Capturing  -------------------------------------------
