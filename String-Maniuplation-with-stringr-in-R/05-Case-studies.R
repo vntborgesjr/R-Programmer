36646897c121c266a2abbe7dd0f71c9a86f321d6
Vitor Borges Jr <vntborgesjr@gmail.com>
2020-12-21 15:34:32 -0300

    Update 05-Case-studies.R

--- String-Maniuplation-with-stringr-in-R/05-Case-studies.R
+++ String-Maniuplation-with-stringr-in-R/05-Case-studies.R
@@ -8,5 +8,13 @@
 # Getting the play into R
 library(stringi)
 library(stringr)
+
 # Read play in using stri_read_lines()
 earnest <- stri_read_lines("Datasets/earnest_file.txt")
+
+# Detect start and end lines
+start <- str_which(earnest, ___)
+end <- str_which(earnest, ___)
+
+# Get rid of gutenberg intro text
+earnest_sub  <- ___
