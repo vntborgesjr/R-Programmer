d682c06d381789bc8714ea068d0e7da23341f7cf
Vitor Borges Jr <vntborgesjr@gmail.com>
2020-12-23 18:31:20 -0300

    Delete 03-Arithmetic-with-Dates-and-Times.R

--- Work-with-Dates-and-Times-in-R/03-Arithmetic-with-Dates-and-Times.R
+++
@@ -1,7 +0,0 @@
-# --------------------------------------------------- 
-# Working with Dates and Times in R - Arithmetic with Dates and Times
-# 22 dez 2020 
-# VNTBJR 
-# --------------------------------------------------- 
-#
-# Taking differences of datetimes -------------------------------------------------
